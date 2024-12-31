cce276e825c8ec16fc13211eac58a4fc3f7976d3
elson43 <elsonsilvanus@gmail.com>
2024-12-31 17:39:34 +0700

    new

--- javascript/password-gate.js
+++ javascript/password-gate.js
@@ -1,14 +1,14 @@
 const PASSWORD_TIMEOUT = 10 * 60 * 1000; // Timeout in milliseconds (e.g., 10 minutes)
 
 // Check if password has been entered and if it's still valid
-const storedTime = sessionStorage.getItem('timestamp');
+const storedTime = localStorage.getItem('timestamp');
 const currentTime = new Date().getTime();
 
 if (!storedTime || currentTime - storedTime > PASSWORD_TIMEOUT) {
     alert('Session expired. Please re-enter the password.');
-    sessionStorage.clear(); // Clear the sessionStorage
+    localStorage.clear(); // Clear the localStorage
     window.location.href = 'index.html'; // Redirect to the password page
 } else {
     // Update the timestamp to extend the session
-    sessionStorage.setItem('timestamp', currentTime);
+    localStorage.setItem('timestamp', currentTime);
 }
